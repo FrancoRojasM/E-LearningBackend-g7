0d377688f429e25f89e99f142d30f274ae5e839f
FrancoRM <franco_rojas_maravi@hotmail.com>
2022-08-03 05:35:51 -0500

    avance prisma

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import { PrismaConnector } from './prisma.js'
+
+const app=express()
+const PORT=process.env.PORT
+
+app.get('/departamentos', async(req,res)=>{  
+    try{
+    //SELECT * FROM departamentos;
+    const resultado=await PrismaConnector.departamento.findMany().then((resultado)=>{
+        console.log(resultado);
+    }).catch((razon)=>{
+        console.error(razon);
+    });
+    console.log("hola");
+    return res.json({
+        message:"Hola",
+    });
+    } catch(razon){
+        return res.json({
+        message:"Algo salio mal",
+        });
+    }
+});
+
+app.listen(PORT,()=>{
+    console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`); //Backtips    
+})
