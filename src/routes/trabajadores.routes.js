d2d9d2b2ca4b9ff629e249639d4d3da338f091a1
FrancoRM <franco_rojas_maravi@hotmail.com>
2022-08-03 22:11:26 -0500

    agregué start

--- src/routes/trabajadores.routes.js
+++ src/routes/trabajadores.routes.js
@@ -2,11 +2,16 @@ import {Router} from 'express';
 import { 
     cambiarPassword, 
     postTrabajador, 
-    validarTrabajador 
+    validarTrabajador,
+    login,
+    perfil
 } from '../controllers/trabajadores.controllers.js'; 
 
+import {verificarToken, isGerente} from '../utils/validador.js'
 export const trabajadoresRouter=Router();
 
-trabajadoresRouter.post("/registro",postTrabajador);
+trabajadoresRouter.post("/registro",verificarToken,isGerente,postTrabajador);
 trabajadoresRouter.post("/validar-trabajador",validarTrabajador);
 trabajadoresRouter.post("/cambiar-password",cambiarPassword);
+trabajadoresRouter.post("/login",login);
+trabajadoresRouter.get("/me",verificarToken,perfil);
